01c3d7559f4a5b473b6cc1a5485e845fd9e9f7aa
SebiPr <Se.Herrmann1@gmail.com>
2017-03-13 10:26:39 +0100

    +HTML

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+ "use strict";
+ $(document).ready(function (){ 
+
+	// Open menu on tablet/mobile
+	$('.menu-btn').on('click', function(){
+		$(this).toggleClass('opened');
+		$('.main-nav').toggleClass('opened');   
+	});
+
+	// Slider initialization
+	if ($.isFunction($.fn.slick)) {
+		$('.carousel').slick({
+			dots: true,
+			arrows: false,
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			responsive: [
+			{
+				breakpoint: 1200, 
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 544,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: true
+				}
+			}]
+		});
+	}
+
+	// Main menu links scroll
+	$('.main-nav').on('click', 'a', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: $(this.getAttribute('href')).offset().top
+		}, 500);
+		$('.menu-btn, .main-nav').removeClass('opened');
+	});
+
+	// Scroll link
+	$('.scroll-indicator').on('click', 'a', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: $('.description').offset().top
+		}, 500);
+	});
+
+	$('.main-header .header-inner').on('click', 'a', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: $(this.getAttribute('href')).offset().top
+		}, 500);
+	});
+
+});
